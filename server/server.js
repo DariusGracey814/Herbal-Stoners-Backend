39cefee40cfff39c9dd3450829cd6966a5701e07
DariusGracey <Dariusgracey814@gmail.com>
2023-04-17 17:21:40 -0400

    update express static directory

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
+app.use(express.static("../public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
