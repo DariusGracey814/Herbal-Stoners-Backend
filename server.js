37cf7f499e5a7f4a76ab487f780a5583db6f0b0f
DariusGracey <Dariusgracey814@gmail.com>
2023-04-18 12:51:09 -0400

    update server js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const router = express.Router();
 
 app.use(cors());
-app.use(express.static("../dist"));
+app.use(express.static("/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
