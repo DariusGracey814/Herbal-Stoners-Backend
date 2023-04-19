05ff9f9de782021ba20c95461c636688eab33ed1
DariusGracey <Dariusgracey814@gmail.com>
2023-04-18 22:30:40 -0400

    update server cors

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const corsOptions = require("./config/corsOptions");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(cors(corsOptions));
 // app.use(express.static("dist"));
 app.use(express.json());
