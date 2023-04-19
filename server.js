6f96033483759913284a8a17c95d4d80171a9f4a
DariusGracey <Dariusgracey814@gmail.com>
2023-04-18 22:46:42 -0400

    update  server js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(cors(corsOptions));
 // app.use(express.static("dist"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
