b696dda5ef5b7b6bf54a05f64c839d223e0dd7a8
DariusGracey <Dariusgracey814@gmail.com>
2023-04-18 22:49:55 -0400

    update  server js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const stripe = require("stripe")(`${process.env.STRIPE_API_KEY}`);
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const corsOptions = require("./config/corsOptions");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 // app.use(express.static("dist"));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Server is running....");
